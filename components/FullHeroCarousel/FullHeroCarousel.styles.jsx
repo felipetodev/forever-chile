fa262f0a4d832a73a430a67ebfae98dd8b7a4a78
felipetodev <fe.ossandon.u@gmail.com>
2022-03-21 19:55:23 -0300

    feat: add full hero video

---
+++ components/FullHeroCarousel/FullHeroCarousel.styles.jsx
@@ -0,0 +1,125 @@
+import styled from "styled-components"
+
+export const FullHeroStyled = styled.div`
+  display: flex;
+  position: relative;
+  max-height: 70vh;
+
+  // Mobile view
+  @media(max-width: 400px) {
+    min-height: 400px;
+  }
+`
+
+export const Video = styled.video`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`
+
+export const Container = styled.div`
+  position: absolute;
+  display: grid;
+  grid-template-columns: 1.5fr 1fr 1fr;
+  align-items: flex-end;
+  margin-left: 40px;
+  top: 0;
+  bottom: 0;
+  background: none;
+  overflow: hidden;
+
+    // Mobile view
+  @media(max-width: 400px) {
+    grid-template-columns: 1fr;
+    margin-left: 0px;
+    right: 0;
+  }
+`
+
+export const LogoStyled = styled.div`
+  padding-left: 40px;
+  background: none;
+
+  // Mobile view
+  @media(max-width: 400px) {
+    padding-left: 100px;
+  }
+`
+
+export const IntroStyled = styled.div`
+  position: relative;
+  color: transparent;
+  min-width: 570px;
+  background: none;
+  z-index: 1;
+  
+  &::before {
+    content: '';
+    position: absolute;
+    background: transparent url('/line.png') 0% 0% repeat padding-box;
+    left: -30px;
+    top: -800px;
+    height: 1200px;
+    width: 1px;
+  }
+
+  div {
+    background: none !important;
+    writing-mode: vertical-rl;
+    -webkit-transform: rotate(180deg);
+    transform: rotate(180deg);
+    white-space: nowrap;
+    cursor: pointer;
+
+    h2 {
+      position: relative;
+      transition: transform 150ms ease;
+    }
+
+    span {
+      transition: visibility 60ms ease-out;
+    }
+
+    & > * {
+      background: none;
+    }
+    &:hover {
+      h2::after {
+        content:'SEE PROJECT';
+        top: 0;
+        right: 40px;
+        font-size: 12px;
+        position: absolute;
+      }
+      h2 {
+        transform: scale(1.3) translateY(39.5px);
+      }
+      span {
+        visibility: hidden;
+      }
+    }
+  }
+
+  // Tablet view
+  @media(max-width: 869px) {
+    min-width: 100%;
+    &::before {
+      display: none;
+    }
+  }
+
+  // Mobile view
+  @media(max-width: 400px) {
+    pointer-events: none;
+    div {
+      // margin-right: 20px;
+      margin-bottom: 20px;
+      h2 {
+        font-size: 12px;
+      }
+      span {
+        font-size: 10px;
+      }
+    }
+  }
+`
---
+++ components/FullHeroCarousel/index.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Navigation, Pagination } from "swiper";
+import {
+  FullHeroStyled,
+  Video,
+  Container,
+  LogoStyled,
+  IntroStyled,
+} from "./FullHeroCarousel.styles";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const videos = [{ url: "/video2.mp4", autoPlay: true }, { url: "/video.mp4" }];
+
+const getActiveSlide = () => {
+  const activeSlider = document.querySelector(".swiper-slide-active").lastChild
+  if (activeSlider) activeSlider.play()
+}
+
+const FullHeroCarousel = () => {
+  const [index, setIndex] = useState(null)
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.defaultMuted = true;
+  }, [])
+
+  useEffect(() => {
+    getActiveSlide()
+  }, [index])
+
+  return (
+    <FullHeroStyled>
+      <Swiper
+        onTransitionStart={e => setIndex(e.realIndex)}
+        effect="fade"
+        pagination={{ clickable: true }}
+        modules={[EffectFade, Navigation, Pagination]}
+      >
+        {videos.map((video, idx) => (
+          <SwiperSlide key={video.url}>
+            <Video ref={videoRef} id={`player${idx}`} playsInline muted loop>
+              <source src={video.url} type="video/mp4" />
+            </Video>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Container>
+        <LogoStyled />
+        <IntroStyled>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h2>Aftershock</h2>
+            <span>Chiloé Films</span>
+            <span>Special Effects, Set Design, Set Construction.</span>
+          </div>
+        </IntroStyled>
+      </Container>
+    </FullHeroStyled>
+  );
+};
+
+export default FullHeroCarousel;
