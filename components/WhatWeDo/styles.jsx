1ce88164884375b4d3fd97094d2c5cfcd829c3d4
felipetodev <fe.ossandon.u@gmail.com>
2022-03-26 23:37:36 -0300

    feat: add section responsive

---
+++ components/WhatWeDo/styles.jsx
@@ -0,0 +1,92 @@
+import Layout from "../Layout";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  margin-top: 200px;
+  padding-bottom: 200px;
+  z-index: 4;
+`
+
+export const LayoutStyled = styled(Layout)`
+  @media (max-width: 740px) {
+    .div3 {
+      display: none;
+    }
+  }
+`
+
+export const Col = styled.div`
+  display: flex;
+  width: 100%;
+  font-size: clamp(.9rem,3.5vw, 2.3rem);
+  @media (max-width: 400px) {
+    justify-content: center;
+  }
+`
+
+export const NavigationList = styled.ul`
+  background: none;
+
+  & li {
+    display: block;
+    user-select: none;
+    margin: 30px 0;
+    & span {
+      background: none;
+      position: relative;
+      z-index: 4;
+      display: block;
+
+      &::before {
+        content: attr(data-text);
+        width: 0;
+        color: crimson;
+        font-weight: 400;
+        // -webkit-text-stroke: 1px;
+        overflow: hidden;
+        position: absolute;
+        transition: all 1s cubic-bezier(0.84, 0, 0.08, 0.99);
+      }
+
+      &:hover:before {
+        width: 100%;
+      }
+    }
+  }
+
+  li:hover .image-container {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  .image-container {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: -75px;
+    left: 0;
+    z-index: 1;
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.7s ease-in-out;
+    img {
+      background: none;
+    }
+  }
+
+  @media (max-width: 740px) {
+    justify-content: center;
+    padding: 0;
+    li {
+      span {
+        &::before {
+          display: none;
+        }
+      }
+    }
+    .image-container {
+      display: none;
+    }
+  }
+`;
