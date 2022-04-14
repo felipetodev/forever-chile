748c11545f9e49f5a2bb597fe23a9b243b745299
felipetodev <fe.ossandon.u@gmail.com>
2022-04-14 16:57:43 -0400

    change: add svg instead png

--- components/HoverBanner/index.jsx
+++ components/HoverBanner/index.jsx
@@ -1,18 +1,23 @@
+import Link from "next/link"
 import { Container, First, HoverWrapper, Second } from "./styles";
 
 const HoverBanner = () => {
   return (
     <Container>
-      <HoverWrapper>
-        <First className="hover-one">
-          <div>{"Let's start"}&nbsp;</div>
-          <div>a project</div>
-        </First>
-        <Second className="hover-two">
-          <div>that last&nbsp;</div>
-          <div>forever.</div>
-        </Second>
-      </HoverWrapper>
+      <Link href="/contact">
+        <a>
+          <HoverWrapper>
+            <First className="hover-one">
+              <div>{"Let's start"}&nbsp;</div>
+              <div>a project</div>
+            </First>
+            <Second className="hover-two">
+              <div>that last&nbsp;</div>
+              <div>forever.</div>
+            </Second>
+          </HoverWrapper>
+        </a>
+      </Link>
     </Container>
   );
 };
