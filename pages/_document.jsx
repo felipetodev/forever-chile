3406216a71d6675a5304b6262807cab3365055eb
felipetodev <fe.ossandon.u@gmail.com>
2022-04-14 16:50:46 -0400

    feat: add new fonts pragmatica

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href="https://use.typekit.net/qii7ovl.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
